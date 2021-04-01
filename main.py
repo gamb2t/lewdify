fb53558d7051b49b762c53b5e72175c89e64c8b3
gamb1t <74240139+gamb2t@users.noreply.github.com>
2021-04-02 02:55:40 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,235 @@
+import discord, asyncio,requests
+import config
+from discord.ext import commands, tasks
+from discord.ext.commands import Bot
+
+prefix = ">"
+token = "ODI3MjMxNTYzMTY2NTE1MjAy.YGYBbQ.xpDiSYmTgstdWEs92m09JMdpE-o"
+
+
+
+
+bot = Bot(command_prefix=prefix)
+
+
+@bot.command()
+async def anal(ctx):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/anal")
+    res = r.json()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"anal.gif"))
+    except:
+        em = discord.Embed()
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
+
+@bot.command()
+async def erofeet(ctx):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/erofeet")
+    res = r.json()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"erofeet.png"))
+    except:
+        em = discord.Embed()
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
+
+@bot.command()
+async def feet(ctx):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/feetg")
+    res = r.json()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"feet.gif"))
+    except:
+        em = discord.Embed()
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
+
+@bot.command()
+async def hentai(ctx):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/Random_hentai_gif")
+    res = r.json()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"hentai.gif"))
+    except:
+        em = discord.Embed()
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
+
+@bot.command()
+async def boobs(ctx):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/boobs")
+    res = r.json()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"boobs.gif"))
+    except:
+        em = discord.Embed()
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
+
+@bot.command()
+async def tits(ctx):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/tits")
+    res = r.json()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"tits.gif"))
+    except:
+        em = discord.Embed()
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
+
+@bot.command()
+async def blowjob(ctx):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/blowjob")
+    res = r.json()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"blowjob.gif"))
+    except:
+        em = discord.Embed()
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
+
+@bot.command(aliases=["neko"])
+async def lewdneko(ctx):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/nsfw_neko_gif")
+    res = r.json()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"neko.gif"))
+    except:
+        em = discord.Embed()
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
+
+@bot.command()
+async def lesbian(ctx):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/les")
+    res = r.json()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"lesbian.gif"))
+    except:
+        em = discord.Embed()
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
+
+@bot.command()
+async def cumslut(ctx):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/cum")
+    res = r.json()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"cumslut.gif"))
+    except:
+        em = discord.Embed()
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
+
+@bot.command(aliases=["vagina"])
+async def pussy(ctx):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/pussy")
+    res = r.json()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"pussy.gif"))
+    except:
+        em = discord.Embed()
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
+
+@bot.command()
+async def waifu(ctx):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/waifu")
+    res = r.json()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file=discord.File(file, f"waifu.gif"))
+    except:
+        em = discord.Embed()
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
+
+@bot.command()
+async def feed(ctx, user: discord.Member):
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/feed")
+    res = r.json()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(res['url']) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(user.mention, file=discord.File(file, f"feed.gif"))
+    except:
+        em = discord.Embed(description=user.mention)
+        em.set_image(url=res['url'])
+        await ctx.send(embed=em)
+
+bot.run(token)
